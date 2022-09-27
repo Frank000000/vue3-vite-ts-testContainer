3579958a2bd9d0dc8be691bf13ce51895616c78f
Frank000000 <2469739226@qq.com>
2022-09-27 09:32:59 +0800

    chore:Cool Animation

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import router from "./router";
 import { Button,RadioGroup, Radio } from 'vant';
 import 'vant/lib/index.css';
 import store from './store'
+// import './style.css'
+import 'selleri-ui/dist/style.css'
 
 const app =createApp(App)
 app.use(router).use(store).use(Button).use(RadioGroup).use(Radio)
