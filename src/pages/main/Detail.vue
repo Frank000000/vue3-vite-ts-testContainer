2c3b97f38ecf0c91e9dba6ac7ff1d939dc1238e9
Frank000000 <2469739226@qq.com>
2022-10-10 10:28:42 +0800

    fix:update vue config

--- src/pages/main/Detail.vue
+++ src/pages/main/Detail.vue
@@ -2,9 +2,9 @@
   <div>详情页</div>
   <van-button type="primary" @click="refreshPage">刷新</van-button>
   <test-component></test-component>
-  <!-- <div class="card">
+  <div class="card">
     Magic Card
-  </div> -->
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -97,4 +97,5 @@ a {
   font-weight: bold;
   margin-top: 2rem;
 }
+
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,11 @@ import {
   defineConfig
 } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import styleImport from 'vite-plugin-style-import';
 import path from 'path';
 
 export default defineConfig({
   plugins: [
-    vue(),
-    // styleImport({
-    //   libs: [{
-    //     libraryName: 'vant',
-    //     esModule: true,
-    //     resolveStyle: (name) => `vant/es/${name}/style/index`,
-    //   }, ],
-    // }),
+    vue()
   ],
   base:"./",//打包路径
   resolve: {
@@ -42,64 +34,3 @@ export default defineConfig({
     cors:true
   }
 })
-
-
-
-// import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
-// const fs = require("fs")
-// const dotenv = require("dotenv")
-// const { resolve } = require('path') // 编辑器提示 path 模块找不到，可以yarn add @types/node --dev
-
-// export default ({ command, mode }) =>{
-// 	let NODE_ENV =  process.env.NODE_ENV || 'development';
-// 	let envFiles = [];
-// 	//根据不同的环境使用不同的环境变量
-// 	if(command == 'serve'){
-// 		envFiles = [
-// 			/** default file */
-// 			`.env`
-// 		]
-// 	}else{
-// 		envFiles = [
-// 			/** default file */
-// 			`.env`,
-// 			/** mode file */
-// 			`.env.${NODE_ENV}`
-// 		]
-// 	}
-// 	for (const file of envFiles) {
-// 		const envConfig = dotenv.parse(fs.readFileSync(file))
-// 		for (const k in envConfig) {
-// 			process.env[k] = envConfig[k]
-// 		}
-// 	}
-// 	return defineConfig({
-// 		plugins: [vue()],
-// 		resolve: {
-// 		    alias: {
-// 		      '@': resolve(__dirname, 'src') // 设置 `@` 指向 `src` 目录
-// 		    }
-// 		},
-// 		server: {
-// 			host: process.env.VITE_HOST,
-// 			port: process.env.VITE_PORT,
-// 			// 是否自动在浏览器打开
-// 			open: true,
-// 			// 是否开启 https
-// 			https: false,
-// 			// 服务端渲染
-// 			ssr: false,
-// 			base: process.env.VITE_BASE_URL,
-// 			outDir: process.env.VITE_OUTPUT_DIR,
-// 			proxy: {
-// 			  '/api': {
-// 				target: 'http://api网关所在域名',
-// 				changeOrigin: true,
-// 				rewrite: (path) => path.replace(/^\/api/, '')
-// 			  },
-// 			}
-// 		},
-// 	})
-// }
-
