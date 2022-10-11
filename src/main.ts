55ceed4962f420089579e27f3b853645baea9132
Frank000000 <2469739226@qq.com>
2022-10-11 10:42:52 +0800

    chore:updat

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import router from "./router";
 import { Button,RadioGroup, Radio } from 'vant';
 import 'vant/lib/index.css';
 import store from './store'
-// import 'selleri-ui/dist/style.css'
+import 'selleri-ui/dist/style.css'
 
 const app =createApp(App)
 app.use(router).use(store).use(Button).use(RadioGroup).use(Radio)
